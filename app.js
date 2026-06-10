ed7791929b398d342d9a3ad0d96f811550fa294d
ajay-nimap <ajay.more@nimapinfotech.com>
2026-06-10 15:07:50 +0530

    change in port

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ const server = http.createServer((req, res) => {
 });
 const PORT = 3001;
 
-server.listen(PORT, () => {
+server.listen(PORT,'0.0.0.0' ,() => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
